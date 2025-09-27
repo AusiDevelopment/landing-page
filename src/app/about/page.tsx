c41de4fe776da0aa9fe1a329c817ac5693faa83b
AustrianNoah <marcel.w@friendlyhost.email>
2025-09-27 21:55:19 +0200

    added about page

---
+++ src/app/about/page.tsx
@@ -0,0 +1,17 @@
+
+
+export default function AboutPage() {
+
+    return (
+
+        <>
+
+            <div className="flex justify-between items-center w-full max-w-7xl">
+                <h1 className="text-2xl font-bold text-gray-900">Hey</h1>
+            </div>
+
+        </>
+
+    )
+
+}
